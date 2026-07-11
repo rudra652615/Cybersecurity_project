dc8701a992f81fd1ca409c1369458b6eba9e2a3e
rudra652615 <266802061+rudra652615@users.noreply.github.com>
2026-07-11 21:33:14 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,1 @@
 console.log("SOCMINT-X System Loaded");
-const button = document.querySelector("button");
-button.addEventListener("click",()=>{
-    alert(
-        "AI Intelligence Module will be connected in future versions."
-    );
-});
